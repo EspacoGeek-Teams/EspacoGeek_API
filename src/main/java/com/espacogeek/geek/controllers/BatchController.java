e5c12476a81f04742a0d93b19a02257f2757be50
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-02-17 23:19:28 -0300

    Add endpoint to restart batch jobs with error handling

--- src/main/java/com/espacogeek/geek/controllers/BatchController.java
+++ src/main/java/com/espacogeek/geek/controllers/BatchController.java
@@ -1,21 +1,21 @@
 package com.espacogeek.geek.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,6 +45,23 @@ public class BatchController {
 
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Erro: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/restart/{executionId}")
+    @PreAuthorize("hasRole('admin')")
+    public ResponseEntity<String> runJob(@PathVariable Long executionId) {
+        try {
+            Long newExecutionId = jobOperator.restart(executionId);
+
+            return ResponseEntity.ok("Job reiniciado! Novo Execution ID: " + newExecutionId);
+
+        } catch (NoSuchJobExecutionException e) {
+            return ResponseEntity.status(404).body("ID de execução não encontrado.");
+        } catch (JobInstanceAlreadyCompleteException e) {
+            return ResponseEntity.status(400).body("Este Job já foi concluído com sucesso e não pode ser reiniciado.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Erro ao reiniciar: " + e.getMessage());
         }
     }
 
