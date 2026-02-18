304d9128788a055f4af3bb37fbdb3b553b88db90
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-02-17 23:14:35 -0300

    Add DelayChunkListener to introduce a delay after chunk processing in batch jobs

---
+++ src/main/java/com/espacogeek/geek/batch/DelayChunkListener.java
@@ -0,0 +1,26 @@
+package com.espacogeek.geek.batch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.batch.core.ChunkListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+
+@Slf4j
+public class DelayChunkListener implements ChunkListener {
+    private final long delayInMillis;
+
+    public DelayChunkListener(long delayInMillis) {
+        this.delayInMillis = delayInMillis;
+    }
+
+    @Override
+    public void afterChunk(@NotNull ChunkContext context) {
+        try {
+            log.info("Chunk completed successfully. Applying delay of {}ms...", delayInMillis);
+            Thread.sleep(delayInMillis);
+        } catch (InterruptedException e) {
+            log.error("Delay thread interrupted", e);
+            Thread.currentThread().interrupt();
+        }
+    }
+}
