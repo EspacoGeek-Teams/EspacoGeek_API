8af6252da8c3dece77384ed862da11fa12e3f282
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-02-19 16:01:29 -0300

    Simplify JPA imports in PeopleModel.java
    Removed individual import statements for JPA annotations and replaced them with a wildcard import.

--- src/main/java/com/espacogeek/geek/models/PeopleModel.java
+++ src/main/java/com/espacogeek/geek/models/PeopleModel.java
@@ -3,16 +3,7 @@ package com.espacogeek.geek.models;
 import java.io.Serializable;
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
-
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
