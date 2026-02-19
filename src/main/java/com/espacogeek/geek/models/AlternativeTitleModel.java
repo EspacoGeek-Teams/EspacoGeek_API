f679648079d2d4df0016300fb7c395c2d85640e4
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-02-19 16:00:30 -0300

    Refactor AlternativeTitleModel by cleaning imports
    Removed unnecessary imports and simplified the table annotation.

--- src/main/java/com/espacogeek/geek/models/AlternativeTitleModel.java
+++ src/main/java/com/espacogeek/geek/models/AlternativeTitleModel.java
@@ -2,22 +2,14 @@ package com.espacogeek.geek.models;
 
 import java.io.Serializable;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name = "alternative_titles", indexes = { @Index(name = "idx_title", columnList = "name_title", unique = false) })
+@Table(name = "alternative_titles")
+// indexes = { @Index(name = "idx_title", columnList = "name_title", unique = false) }
 @Entity
 @Getter
 @Setter
