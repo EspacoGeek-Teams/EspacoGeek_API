01d7e25ef60608143cea33a451865598c6453c05
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-04-15 20:27:27 -0300

    chore: Remove unused imports from QuoteApiImpl

--- src/main/java/com/espacogeek/geek/data/api/impl/QuoteApiImpl.java
+++ src/main/java/com/espacogeek/geek/data/api/impl/QuoteApiImpl.java
@@ -9,9 +9,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
